e99cfa021ba05f8a5ed41bf57d011a7a4f00fc9d
J.M. Vishal Rashmika Perera <87840726+VishalRashmika@users.noreply.github.com>
2025-08-16 11:40:13 +0530

    AST Generator

--- tools/GenerateAST.cpp
+++ tools/GenerateAST.cpp
@@ -9,6 +9,7 @@ const std::string baseFileName = "Expr";
 //forward decleration
 void ASTDefinition(std::string outputPath, std::string baseFileName, std::vector<std::string> expressionTypes);
 void visitorDefinition(std::ofstream& fileWriter, std::vector<std::string>& expressionTypes);
+void typeDefinition(std::ofstream&, std::string expressionName, std::string expressionContent);
 
 int main(int argc, char* argv[]){
     // todo: input validation : should have atleast one parameter
@@ -22,7 +23,7 @@ int main(int argc, char* argv[]){
     };
 
     ASTDefinition(outputPath, baseFileName, exprTypes);
-    std::cout << "end of program" << std::endl;
+    std::cout << baseFileName << ".h Generated Successfully!!!" << std::endl;
 }
 
 
@@ -32,7 +33,7 @@ void ASTDefinition(std::string outputPath, std::string baseFileName, std::vector
 
     fileWriter << "#pragma once\n" << std::endl;
     fileWriter << "#include \"Token.h\"\n" << std::endl;
-    fileWriter << "#include <iostream>\n#include <vector>\n#include <memory>\n#include <any>\n" << std::endl;
+    fileWriter << "#include <any>\n" << std::endl;
 
     for (int i = 0; i < expressionTypes.size(); i++){
         std::string exprClassName = trim(split(expressionTypes[i], ": ")[0]);
@@ -42,7 +43,23 @@ void ASTDefinition(std::string outputPath, std::string baseFileName, std::vector
     fileWriter << std::endl;
 
     // visitor code
+    visitorDefinition(fileWriter, expressionTypes);
 
+    // The base class
+    fileWriter << "class " << baseFileName << "{" << std::endl;
+    fileWriter << "public:" << std::endl;
+    fileWriter << "\tvirtual std::any Accept(Visitor* visitor) = 0;" << std::endl;
+    fileWriter << "\tvirtual ~" << baseFileName << "(){}" << std::endl;
+    fileWriter << "};" << std::endl;
+    fileWriter << std::endl;
+
+    // different AST classes
+    for(int i = 0; i < expressionTypes.size(); i++){
+        std::string expressionName = trim(split(expressionTypes[i], ": ")[0]);
+        std::string expressionContent = trim(split(expressionTypes[i], ": ")[1]);
+
+        typeDefinition(fileWriter, expressionName, expressionContent);
+    }    
 
 
     fileWriter.close();
@@ -50,11 +67,43 @@ void ASTDefinition(std::string outputPath, std::string baseFileName, std::vector
 
 
 void visitorDefinition(std::ofstream& fileWriter, std::vector<std::string>& expressionTypes){
-    fileWriter << "class " << baseFileName << "Visitor{" << std::endl;
+    fileWriter << "class Visitor{" << std::endl;
+    fileWriter << "public:" << std::endl;
 
     for (int i = 0; i < expressionTypes.size(); i++){
         std::string visitExprName = trim(split(expressionTypes[i], ": ")[0]);
         
-        fileWriter << "\tvirtual std::any visit" << visitExprName << baseFileName << "(std::shared_ptr<" << visitExprName << "> " << toLower(baseFileName) << ") = 0;\n";
+        fileWriter << "\tvirtual std::any visit" << baseFileName << visitExprName;
+        fileWriter << "(" << visitExprName << "* expr) = 0;" << std::endl;
     }
+
+    fileWriter << "};" << std::endl;
+    fileWriter << std::endl;
+}
+
+void typeDefinition(std::ofstream& fileWriter, std::string expressionName, std::string expressionContent){
+    fileWriter << "class " << expressionName << ": public " << baseFileName << "{" << std::endl;
+    fileWriter << "public:" << std::endl;
+
+    std::vector<std::string> fieldsContent = split(expressionContent, ", ");
+    for (int i = 0; i < fieldsContent.size(); i++){
+        fileWriter << "\t" << fieldsContent[i] << ";" << std::endl;
+    }
+    fileWriter << std::endl;
+
+    fileWriter << "\t" << expressionName << "(" << expressionContent << "){" << std::endl;
+    for (int i = 0; i < fieldsContent.size(); i++){
+        std::string varName = split(fieldsContent[i], " ")[1];
+        fileWriter << "\t\tthis->" << varName << " = " << varName << ";" << std::endl;
+    }
+    fileWriter << "\t}" << std::endl;
+    fileWriter << std::endl;
+
+    fileWriter << "\tstd::any Accept(Visitor* visitor) override{" << std::endl;
+    fileWriter << "\t\treturn visitor->visit" << baseFileName << expressionName;
+    fileWriter << "(this);" << std::endl;
+    fileWriter << "\t}" << std::endl; 
+
+    fileWriter << "};" << std::endl;
+    fileWriter << std::endl;
 }
